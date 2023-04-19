416143772e32d52521a24456259c91a66a0de2b2
Aerothises <63618221+Aerothises@users.noreply.github.com>
2023-04-18 19:22:25 -0600

    add contact form
    missing styling of contact form.
    
    Todo: add styling to contact form

---
+++ script_main.js
@@ -0,0 +1,7 @@
+function openSideNav() {
+    document.getElementById("side-menu").style.width = "33vw";
+  }
+  
+  function closeSideNav() {
+    document.getElementById("side-menu").style.width = "0";
+  }
